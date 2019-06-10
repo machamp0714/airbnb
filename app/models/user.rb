510222fd952b5133a768a3d3b64f13e1e19d62ae
25 macamp <rateliasudaisuki@gmail.com>
2019-06-10 22:54:27 +0900

    rememberメソッドを追加 #5

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save { self.email = email.downcase }
 
   has_secure_password
@@ -14,4 +16,18 @@ class User < ApplicationRecord
   validates :password,
     presence: true,
     length: { minimum: 8 }
+
+    def self.digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+    def self.new_token
+      SecureRandom.urlsafe_base64
+    end
+
+    def remember
+      self.remember_token = User.new_token
+      update_attribute(:remember_digest, User.digest(self.remember_token))
+    end
 end
