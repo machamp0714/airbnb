dd37438342b1b01c32164bdf10b6b44dc5f8bee5
25 macamp <rateliasudaisuki@gmail.com>
2019-06-10 19:33:15 +0900

    ユーザー登録機能を作成 テストも追加 #3

---
+++ spec/features/users_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.feature 'users', type: :feature do
+  let(:user) { FactoryBot.build(:alice) }
+
+  scenario 'ユーザー登録できる事' do
+    visit root_path
+    click_link '新規登録'
+    fill_in 'ユーザー名', with: user.name
+    fill_in 'メールアドレス', with: user.email
+    fill_in 'パスワード', with: user.password
+    expect {
+      click_on '登録'
+    }.to change(User, :count).by(1)
+    expect(page).to have_content '登録が完了しました。'
+    expect(current_path).to eq root_path
+  end
+end
