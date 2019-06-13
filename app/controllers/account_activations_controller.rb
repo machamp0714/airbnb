e1bd829ae0571ffc688b83f00f577ae5c286053d
25 macamp <rateliasudaisuki@gmail.com>
2019-06-14 00:34:10 +0900

    AccountActivationsControllerのeditアクションを追加 #9

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,4 +1,15 @@
 class AccountActivationsController < ApplicationController
   def edit
+    user = User.find_by(email: params[:email])
+    if user && user.authenticated?(:activation, params[:id])
+      user.update_attribute(:activated, true)
+      user.update_attribute(:activated_at, Time.zone.now)
+      login user
+      flash[:success] = 'アカウントが有効化されました。'
+      redirect_to root_path
+    else
+      flash[:danger] = 'このリクエストは無効です。'
+      redirect_to root_path
+    end
   end
 end
