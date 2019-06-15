fee1afc498450608ce8c71ffd43d2682a7e729a5
25 macamp <rateliasudaisuki@gmail.com>
2019-06-16 00:07:48 +0900

    Docker内でHeadless Chromeを使ってJSのテストが出来るように修正 #9

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,3 +1,12 @@
-Webdrivers::Chromedriver.required_version = "75.0.3770.90"
+Capybara.register_driver :chrome_headless do |app|
+  options = ::Selenium::WebDriver::Chrome::Options.new
 
-Capybara.javascript_driver = :selenium_chrome
+  options.add_argument('--headless')
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-dev-shm-usage')
+  options.add_argument('--window-size=1400,1400')
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
+
+Capybara.javascript_driver = :chrome_headless
