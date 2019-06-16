bf8a871bd3ec7f1bd5e78b4326660720a30f6451
25 macamp <rateliasudaisuki@gmail.com>
2019-06-16 14:45:51 +0900

    usersテーブルに新しいcolumnを追加 #1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_13_131338) do
+ActiveRecord::Schema.define(version: 2019_06_16_054409) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2019_06_13_131338) do
     t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at"
+    t.string "phone_number"
+    t.text "description"
     t.index ["activation_digest"], name: "index_users_on_activation_digest", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name", unique: true
