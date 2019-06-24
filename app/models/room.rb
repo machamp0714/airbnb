740d8dec5df6dc7b52312f21c151012eba4d4317
25 macamp <rateliasudaisuki@gmail.com>
2019-06-25 00:21:29 +0900

    index viewを作成 #18

--- app/models/room.rb
+++ app/models/room.rb
@@ -11,4 +11,12 @@ class Room < ApplicationRecord
   def is_ready?
     !active && !price.blank? && !listing_name.blank? && !photos.blank? && !address.blank?
   end
+
+  def cover_photo(size)
+    if self.photos.length > 0
+      self.photos[0].image_url(size)
+    else
+      'blank.jpg'
+    end
+  end
 end
