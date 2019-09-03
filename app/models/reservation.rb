68ffac0e939465da489137ad925213e97ed87252
25 macamp <rateliasudaisuki@gmail.com>
2019-09-03 17:41:19 +0900

    Add Callback to Reservation #51

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,16 @@
 class Reservation < ApplicationRecord
   enum status: { Waiting: 0, Approved: 1, Dicline: 2 }
 
+  after_create_commit :create_notification
+
   belongs_to :user
   belongs_to :room
+
+  private
+    def create_notification
+      type = self.room.Instant? ? "New Booking" : "New Request"
+      guest = User.find(self.user_id)
+
+      Notification.create(content: "#{type} from #{guest.name}", user_id: self.room.user_id)
+    end
 end
